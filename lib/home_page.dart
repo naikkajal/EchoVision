c879f7da8ab52e00ca44ed441f37a18f81f8406d
Kajal Naik <naikkajal0603@gmail.com>
2024-05-10 12:52:49 +0530

    recognizing

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,9 +1,58 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chatbot/feature_box.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
-class Homepage extends StatelessWidget {
-  const Homepage({Key? key});
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  late final SpeechToText speechToText;
+  String lastWords = ' ';
+
+  @override
+  void initState() {
+    super.initState();
+    speechToText = SpeechToText();
+    initSpeechToText();
+  }
+
+  Future<void> initSpeechToText() async {
+    if (!await speechToText.initialize()) {
+      print('SpeechToText initialization failed.');
+    }
+    setState(() {});
+  }
+
+  Future<void> startListening() async {
+    await speechToText.listen(onResult: onSpeechResult);
+    setState(() {});
+  }
+
+  Future<void> stopListening() async {
+    await speechToText.stop();
+    setState(() {});
+  }
+
+  void onSpeechResult(SpeechRecognitionResult result) {
+  setState(() {
+    lastWords = result.recognizedWords;
+    print('Recognized Words: $lastWords');
+  });
+}
+
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    speechToText.stop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,25 +81,23 @@ class Homepage extends StatelessWidget {
             ),
           ),
           Container(
-             margin: EdgeInsets.symmetric(horizontal: 10).copyWith(top: 30),
-             decoration: BoxDecoration(
-                          border: Border.all(
-                               color: Colors.black
-                          ),
-                          borderRadius: BorderRadius.only(
-                                topRight: Radius.circular(20),
-                                bottomRight: Radius.circular(20),
-                                bottomLeft: Radius.circular(20),
-                          ),
-               ),
-             child: Container(
-                 padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                 child: Text(
-                      "Hello!! How Can I Help You Today?",
-                       style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-               ),
-         ),
+            margin: EdgeInsets.symmetric(horizontal: 10).copyWith(top: 30),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+                bottomLeft: Radius.circular(20),
+              ),
+            ),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text(
+                "Hello!! How Can I Help You Today?",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
           Container(
             margin: EdgeInsets.only(top: 25),
             child: Align(
@@ -58,8 +105,10 @@ class Homepage extends StatelessWidget {
               child: Padding(
                 padding: const EdgeInsets.only(left: 35),
                 child: Text("Here are a few features",
-                     style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold, )
-                ),
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    )),
               ),
             ),
           ),
@@ -67,15 +116,21 @@ class Homepage extends StatelessWidget {
             padding: const EdgeInsets.only(top: 15),
             child: Column(
               children: const [
-                FeatureBox(color: Color.fromARGB(255, 123, 197, 231),
-                 header: 'ChatGPT', 
-                 description: 'A smarter way to stay organized and informed with ChatGPT'),
-                FeatureBox(color: Color.fromARGB(255, 199, 239, 154),
-                 header: 'Dall-E', 
-                 description: 'Get inspired and stay creative with your personal assistant powered by Dall-E'),
-                FeatureBox(color: Color.fromARGB(255, 66, 187, 209),
-                 header: 'Smart Voice Assistant', 
-                 description: 'Get the best of both words with a voice assistant powered by Ball-E and ChatGPT')
+                FeatureBox(
+                    color: Color.fromARGB(255, 123, 197, 231),
+                    header: 'ChatGPT',
+                    description:
+                        'A smarter way to stay organized and informed with ChatGPT'),
+                FeatureBox(
+                    color: Color.fromARGB(255, 199, 239, 154),
+                    header: 'Dall-E',
+                    description:
+                        'Get inspired and stay creative with your personal assistant powered by Dall-E'),
+                FeatureBox(
+                    color: Color.fromARGB(255, 66, 187, 209),
+                    header: 'Smart Voice Assistant',
+                    description:
+                        'Get the best of both words with a voice assistant powered by Ball-E and ChatGPT')
               ],
             ),
           )
@@ -83,9 +138,17 @@ class Homepage extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Color.fromARGB(255, 171, 236, 239),
-        onPressed: () {},
-      child: Icon(CupertinoIcons.mic),),
+        onPressed: () async {
+          if (await speechToText.hasPermission && speechToText.isNotListening) {
+            await startListening();
+          } else if (speechToText.isListening) {
+            await stopListening();
+          } else {
+            initSpeechToText();
+          }
+        },
+        child: Icon(CupertinoIcons.mic),
+      ),
     );
   }
 }
-
